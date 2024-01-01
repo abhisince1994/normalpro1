cf07ff8faab15dc94872c07f3760df29edf40094
abhisince1994 <abhijeetsapkal2@gmail.com>
2024-01-01 16:40:03 +0530

    third commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Write your code below:
+//getElemntById()
 const newHeading = document.getElementById('main-heading');
 newHeading.textContent='Fruit World'
 newHeading.style.color='orange'
@@ -13,3 +14,13 @@ basketheading.style.color='green'
 const thanks=document.getElementById('thanks');
 thanks.innerHTML='<p>Please visit us again</p>';
 thanks.style.fontWidth='bold';
+
+// Write your code below:
+//use of getElementsByClassNamegi
+const fruit=document.getElementsByClassName('fruit');
+console.log(fruit[1]);
+fruit[2].style.backgroundColor='yellow'
+//fruit[2].style.fontWidth='bold'
+for(let i=0;i<fruit.length;i++){
+  fruit[i].style.fontWeight = 'bold';
+}

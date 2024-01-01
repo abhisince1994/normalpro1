56f4a54c3139fe5854fc7e23fae357fea995534c
abhisince1994 <abhijeetsapkal2@gmail.com>
2024-01-01 13:59:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Write your code below:
+const newHeading = document.getElementById('main-heading');
+newHeading.textContent='Fruit World'
+newHeading.style.color='orange'
+
+const header=document.getElementById('header');
+header.style.backgroundColor='green'
+header.style.borderBottom='3px solid orange';
+
+const basketheading=document.getElementById('basket-heading');
+basketheading.style.color='green'
+
+const thanks=document.getElementById('thanks');
+thanks.innerHTML='<p>Please visit us again</p>';
+thanks.style.fontWidth='bold';
